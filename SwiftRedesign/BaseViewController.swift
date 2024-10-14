c2d79127aaaf64faf24692903cacd2d1c7ee6267
Akash <akash.pradhan@mapmyindia.com>
2024-10-14 11:47:10 +0530

    initial commit

---
+++ SwiftRedesign/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseViewController.swift
+//  SwiftRedesign
+//
+//  Created by rento on 14/10/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setGadgetsSheet()
+    }
+    
+    func setGadgetsSheet() {
+        
+    }
+}
---
+++ SwiftRedesign/GadgetsSheet.swift
@@ -0,0 +1,15 @@
+//
+//  GadgetsSheet.swift
+//  SwiftRedesign
+//
+//  Created by rento on 14/10/24.
+//
+
+import UIKit
+
+class GadgetsSheet: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
